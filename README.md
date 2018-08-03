29303b0eedd55caa769b5ca287699c2650a1b211
Abbe98 <albin.post@gmail.com>
2018-08-03 13:09:49 +0200

    first

---
+++ README.md
@@ -0,0 +1,19 @@
+# Vue Boilerplate
+
+Personal Vue-CLI boilerplate with my prfeared linting settings and favorite packages included.
+
+## Packages
+
+ - axios
+ - vue-router
+ - vuex
+ - vue-skip-to
+
+## Start a New Project
+
+```
+git clone https://github.com/Abbe98/vue-boilerplate.git
+cd vue-boilerplate
+rm -rf .git/
+git init
+```
--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import './registerServiceWorker'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-Vue.config.productionTip = false
+import axios from 'axios';
+import VueSkipTo from 'vue-skip-to';
+
+Vue.config.productionTip = false;
+
+Vue.use(VueSkipTo);
+
+Vue.prototype.$http = {};
+
+// #TODO Example endpoint configuration - change it to your API endpoint or create duplicate ones.
+Vue.prototype.$http.wdqs = axios.create({
+  baseURL: 'https://query.wikidata.org',
+});
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount('#app');
